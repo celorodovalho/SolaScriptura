00e2f462245904af4ed9ee4249ef6864fe4f32fb
Marcelo Rodovalho <marcelo2208@gmail.com>
2017-05-06 00:28:22 -0300

    Start command

--- app/Http/Controllers/Bot/DefaultController.php
+++ app/Http/Controllers/Bot/DefaultController.php
@@ -15,40 +15,12 @@ class DefaultController extends Controller
 {
     public function show()
     {
-        /*$update = new Telegram\Bot\Objects\Update([
-            'update_id' => 9865464654,
-            'message' => [
-                'message_id' => 984546544,
-                'from' => [
-                    'id' => 144068960,
-                    'first_name' => 'Marcelo Rodovalho'
-                ],
-                'date' => 2016,
-                'chat' => [
-                    'id' => 987654654,
-                    'type' => 'private'
-                ],
-
-            ]
-        ]);
-        $teste = Telegram::getCommandBus()->execute('poke', 'moves 25', $update);
-        dump($teste);die;*/
-
-        $api = new PokeApi;
-//        $pokemon = $api->pokemon('oddish');
-        $pokemon = $api->pokemonSpecies('oddish');
-        $pokemon = json_decode($pokemon, true);
-        $response = PokeCommand::evolution($pokemon, $api);
-
-//        $pokemon = json_decode($pokemon, true);
-//        $response = PokeCommand::moves($pokemon);
-        dump($response);die;
         return 'ok';
     }
 
     public function setWebhook()
     {
-        $response = Telegram::setWebhook(['url' => 'https://br796.hostgator.com.br/~marce769/projects/pokemondsbot/public/index.php/297560032:AAH9-NrRxuTi7x_jrpcCZ-y3s8nMWzAu4Ls/webhook']);
+        $response = Telegram::setWebhook(['url' => 'https://marcelorodovalho.com.br/rodovalhos-bot/public/index.php/335603197:AAE9-l0gWZa4vtikwOnMftilpbcHh1isy58/webhook']);
         //$update = Telegram::commandsHandler(true);
         return $response;
     }
