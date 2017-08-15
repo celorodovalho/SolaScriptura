933f2221953088621ab3a272302a32c086a15271
Marcelo Rodovalho <marcelo2208@gmail.com>
2017-08-15 11:31:43 -0300

    Command Reference

---
+++ app/Commands/ReferenceCommand.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Support\Facades\Log;
+use Telegram;
+use Telegram\Bot\Actions;
+use Telegram\Bot\Commands\Command;
+
+class ReferenceCommand extends Command
+{
+    /**
+     * @var string Command Name
+     */
+    protected $name = "ref";
+
+    /**
+     * @var string Command Description
+     */
+    protected $description = "Search Pokémon by name or ID";
+
+    /**
+     * @inheritdoc
+     */
+    public function handle($arguments)
+    {
+        try {
+            // This will update the chat status to typing...
+            $this->replyWithChatAction(['action' => Actions::TYPING]);
+            if (empty($arguments)) {
+                //throw new Telegram\Bot\Exceptions\TelegramOtherException('Você precisa informar o nome ou número do Pokémon, ex: /dex pikachu');
+                $reply_markup = Telegram::forceReply(['selective' => true]);
+                return $this->replyWithMessage([
+                    //'parse_mode' => 'Markdown',
+                    'text' => 'Informe a referencia, ex: Joao 3:16-17' . "\r\n" .
+                        '[/ref]'
+                    ,
+                    'reply_markup' => $reply_markup
+                ]);
+            }
+
+            $response = $this->simpleCurl('https://bible-api.com/' . $arguments, null, ['translation' => 'almeida']);
+
+            if (strlen($response) > 21) {
+                $response = json_decode($response, true);
+            } else {
+                throw new Telegram\Bot\Exceptions\TelegramOtherException('Referencia nao encontrada.');
+            }
+
+
+            $return = ['*' . $response['reference'] . '*' . "\r\n"];
+            foreach ($response['verses'] as $verse) {
+                $return[] = '*' . $verse['verse'] . ')* ' . trim($verse['text']);
+            }
+
+            $this->replyWithMessage([
+                'parse_mode' => 'Markdown',
+                'text' => implode($return),
+                //'reply_markup' => $reply_markup
+            ]);
+        } catch (Telegram\Bot\Exceptions\TelegramOtherException $e) {
+            $this->replyWithMessage([
+                'parse_mode' => 'Markdown',
+                'text' => $e->getMessage()
+            ]);
+            return null;
+        } catch (\Exception $e) {
+            $this->replyWithMessage([
+                'parse_mode' => 'Markdown',
+                'text' => 'Sorry. Try again later.'
+            ]);
+            $response = Telegram::sendMessage([
+                'parse_mode' => 'Markdown',
+                'chat_id' => '144068960',
+                'text' => $e->getMessage()/* . "\r\n" .
+                    $e->getFile() . "\r\n" .
+                    $e->getLine() . "\r\n"*/
+            ]);
+            Log::info('DEX-ERRO1: ' . json_encode($e->getMessage()));
+            Log::info('DEX-ERRO2: ' . $e->getTraceAsString());
+        }
+
+    }
+
+    public function simpleCurl($url, $post = array(), $get = array())
+    {
+        $url = explode('?', $url, 2);
+        if (count($url) === 2) {
+            $temp_get = array();
+            parse_str($url[1], $temp_get);
+            $get = array_merge($get, $temp_get);
+        }
+
+        $ch = curl_init($url[0] . ($get ? '?' . http_build_query($get) : ''));
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        return curl_exec($ch);
+    }
+}
