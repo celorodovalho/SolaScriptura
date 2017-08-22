808d4e7147a4150fc013bfc0391cdecd587e164a
Marcelo Rodovalho <marcelo2208@gmail.com>
2017-08-22 16:46:13 -0300

    Manual "seed"

--- app/Http/Controllers/Api/References.php
+++ app/Http/Controllers/Api/References.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 
 class References extends Controller
 {
-    public function ref ($version, $book, $chapter, $verses)
+    public function ref($version, $book, $chapter, $verses)
     {
         $occurs = \App\Verses::where('version', '=', $version)
             ->where('book', '=', $book)
@@ -29,5 +30,10 @@ class References extends Controller
             $occurs->whereIn('verse', $verseIn->sort()->toArray());
 
         return response()->json($occurs->get());
+    }
+
+    public function books()
+    {
+        return response()->json(\App\Books::where('abbrev', '=', 'gn')->first()->verses()->where('chapter', '=', 1)->get());
     }
 }
